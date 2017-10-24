fdf6f96920ed75b18c4bb889af7c4e5372bd1556
lbusett <lbusett@gmail.com>
2017-10-25 00:26:56 +0200

    Finalized work on reshuffling riceatlas

--- R/pr_mutate_when.R
+++ R/pr_mutate_when.R
@@ -18,16 +18,7 @@
 #' @export
 #' @author Kevin Ushey
 
-mutate_when <- function(data, ...) {
-  dots <- eval(substitute(alist(...)))
-  for (i in seq(1, length(dots), by = 2)) {
-    condition <- eval(dots[[i]], envir = data)
-    mutations <- eval(dots[[i + 1]], envir = data[condition, , drop = FALSE])
-    data[condition, names(mutations)] <- mutations
-  }
-  data
-}
-mutate_when <- function(data, ...) {
+pr_mutate_when <- function(data, ...) {
   dots <- eval(substitute(alist(...)))
   for (i in seq(1, length(dots), by = 2)) {
     condition <- eval(dots[[i]], envir = data)
